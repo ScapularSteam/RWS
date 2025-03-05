a3d62a5169db1f07ebde76f1630773961e3db151
Scapular Steam <86719656+ScapularSteam@users.noreply.github.com>
2025-03-05 12:26:37 +0000

    Added logos
    Created a class with functions which when called, print out Revolution Logos to the console

---
+++ RevolutionWebServices/Graphics.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevolutionWebServices
+{
+
+    // Internal class with methods to print Revolution logos to terminal
+    internal class Logos {     
+        public static void Large_Arrow ()
+        {
+            Console.WriteLine("\r\n\r\n                                                      \r\n                                                      \r\n                                                      \r\n                  @@@@@@@@@@@@@@@@@@                  \r\n                  @@@@@@@@@@@@@@@@@@                  \r\n                           @@@@@@@@@                  \r\n                         @@@@@@@@@@@                  \r\n                        @@@@@@  @@@@                  \r\n                      @@@@@@    @@@@                  \r\n                    @@@@@@      @@@@                  \r\n                  @@@@@@@       @@@@         @@       \r\n                @@@@@@@         @@@@       @@@@@@     \r\n              @@@@@@@                    @@@@@@@      \r\n            @@@@@@@                    @@@@@@@        \r\n          @@@@@@@                     @@@@@@          \r\n         @@@@@@                     @@@@@@            \r\n       @@@@@@                     @@@@@@@             \r\n     @@@@@@@      @@@@@         @@@@@@@               \r\n       @@@        @@@@@       @@@@@@@                 \r\n                  @@@@@     @@@@@@@                   \r\n                  @@@@@   @@@@@@@                     \r\n                  @@@@@  @@@@@@                       \r\n                  @@@@@@@@@@@                         \r\n                  @@@@@@@@@                           \r\n                  @@@@@@@@@@@@@@@@@@                  \r\n                  @@@@@@@@@@@@@@@@@@                  \r\n                  @@@@@@@@@@@@@@@@@@                  \r\n                                                      \r\n                                                      \r\n\r\n");
+        }
+
+        public static void Small_Arrow ()
+        {
+            Console.WriteLine("\r\n\r\n                                  \r\n                                  \r\n           @@@@@@@@@@@@           \r\n                 @@@@@@           \r\n               @@@@ @@@           \r\n             @@@@   @@@           \r\n           @@@@     @@@           \r\n          @@@       @@@    @@@@   \r\n        @@@@             @@@@     \r\n      @@@@             @@@@       \r\n    @@@@             @@@@         \r\n     @     @@@     @@@@           \r\n           @@@   @@@@             \r\n           @@@ @@@@               \r\n           @@@@@@                 \r\n           @@@@@@@@@@@@           \r\n           @@@@@@@@@@@@           \r\n                                  \r\n\r\n");
+        }
+
+        public static void Wordmark ()
+        {
+            Console.WriteLine("       +@@@@@@@@@@+                                                                                                                                   \r\n       *@        @*        *@@@@@@@@@@@@%                                           +@@@@@+                       %@@@@@                              \r\n       +@@@@@    @*        *@           @@%                                         *@   @*               #@@@@%  @     *                             \r\n        @@@   @  @*  +     *@ :  @@@@#    @*                                        *@ : @*               @    @  @@   @#                             \r\n      %@@   @@@  @#@@@@%   *@ :  @   @@ : @#@@@@    @@@@@@@@@%   %@@@#*@@@@    @@@% #@ : @@@@@@*   @@@@@@@@ .: @@@@@@@@@* %@@@    @@@ *@@@@@@@   *@@% \r\n    %@@   @@@*@@@@@@   @=  *@ :  @@@@@+   @@@          @@    @* %@   @@@          @@%@ : @@   -%   @    @   ::    @@   @#@@          @@@   @        @%\r\n  %@@   @@@    @@@   @@    *@ :          @@@   @@@@@@   @@   @@ @    @@    @@@@@   @@@ : @@ : @@   @ :: @@@ :: @@@@@ : @@@   @@@@@    @@ :  @@@@@ :  @\r\n++@   @@@@@@*@@@   @@      *@ :  @@  : @@@@  :          @@@   @@@   @@@ : @@  #@ :  @@ : @@ : @@   @ :: @@@ :: @  *@ : @@  : @#  @@ : @@ : =@  @@ :: @\r\n  @@@@@#@  @@@   @@        *@ :  @@@    @@@  : @@@@@@@@@@%@    @    @%@   @@  @@ :  @@ : @@   :@  @@ :: @@@ :: @  *@ : @@  : @@  @@   @@ : :@  #@ :: @\r\n    *  *@  @   @@          *@ :  @ @@    @@@    @@@    @* @@   @   @@*@     @@*    @@@ : @@     @@@   : @@@     @@@@ : @@@    @@@    -@@ : :@  #@ :: @\r\n       *@    @@@@@+        *@    @  @@    @@@@        @%   @.     @@   %@        @@@%@   @@@@       @   @@@@@     @@   @#@@@        @@@@    @  #@    @\r\n       *@        @*        *@@@@@#   %@@@@@**@@@@@@@@@     *@@@@@@@*     @@@@@@@@   *@@@@@+ %@@@@@@@@@@@@*  %@@@@@@@@@@@+   @@@@@@@@  *@@@@@*  *%@@@@%\r\n       +@@@@@@@@@@+                                                                                                                                   \r\n");
+        }
+    }
+
+    // Internal class with methods to print graphics and diagrams to terminal
+    internal class Graphics
+    {
+
+    }
+
+    // Internal class with methods to print any kind of fancy titles or graphics to the terminal
+    internal class FancyTitles
+    {
+
+    }
+}
--- RevolutionWebServices/Program.cs
+++ RevolutionWebServices/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+namespace RevolutionWebServices;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logos.Large_Arrow();
+        Console.WriteLine("Welcome to Revolution Web Services");
+        Console.WriteLine("\n\n\nHere you can view your schedule, join workshops, and chat with other participants!\n\n\n\n\n\n\n");
+    }
+}
